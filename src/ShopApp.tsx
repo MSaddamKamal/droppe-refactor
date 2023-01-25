a543c0c8c11c19721c5fd5c5c994fe5703d0b2d0
saddam <saddam@vaultspay.ae>
2023-01-25 20:49:58 +0500

    revamp toggleFavourte functionality and integrate Product List in shopApp

--- src/ShopApp.tsx
+++ src/ShopApp.tsx
@@ -1,9 +1,8 @@
-import React, { useState} from 'react'
+import React, { useEffect, useState} from 'react'
 import lodash from 'lodash'
 import Modal from 'react-modal'
 import { FaTimes } from 'react-icons/fa'
 import { Button } from './components/button'
-import ProductList from './components/product-list-components'
 import { Form } from './components/form'
 import logo from './images/droppe-logo.png'
 import img1 from './images/img1.png'
@@ -12,6 +11,8 @@ import styles from './ShopApp.module.css'
 import Header from 'components/Header'
 import Banner from 'components/Banner'
 import Stats from 'components/Stats'
+import ProductList from 'components/Product/ProductList'
+import useRequest from 'hooks/useRequest'
 
 // export class ShopApp extends React.Component<
 // {},
@@ -166,13 +167,47 @@ import Stats from 'components/Stats'
 // }
 
 export const ShopApp = () => {
+
+  const url = 'https://fakestoreapi.com/products' // can be referenced from env or config file as well
+  const { loading, error, errorMsg, data } = useRequest(url)
+  const [productsData, setProductsData] = useState<any[]>([])
   const [productCount, setProductCount] = useState(0)
   const [favCount, setFavCount] = useState(0)
+
+  useEffect(() => {
+    setProductsData(data)
+  }, [data])
+
+  useEffect(() => {
+    setProductCount(productsData.length)
+  }, [productsData])
+
+  
+  const togglefav = (id: number | string) => {
+    let isfav = false
+
+    const updatedData = productsData.map((item) =>
+      item.id == id
+        ? {
+            ...item,
+            isFavourite:
+							typeof item.isFavourite == 'undefined'
+							  ? (isfav = true)
+							  : (isfav = !item.isFavourite)
+				  }
+        : item
+    )
+
+    setProductsData(updatedData)
+    setFavCount((prev) => (isfav ? prev + 1 : prev - 1))
+  }
+
   return (
     <div className='container'>
       <Header />
       <Banner />
       <Stats totalProducts={productCount} favourites={favCount} />
+      <ProductList products={productsData} toggleFav={togglefav} />
     </div>
   )
 }
