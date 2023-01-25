6731d5fb19264fcbdf4da1cef79514424d483a57
saddam <saddam@vaultspay.ae>
2023-01-25 20:32:30 +0500

    created ProductList componebt and export as default

---
+++ src/components/Product/ProductList.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import ProductItem from './ProductItem'
+import { type Product, type ToggleFavroute } from './types'
+
+interface ProductListProps {
+  products: Product[]
+  toggleFav: ToggleFavroute
+}
+
+
+export const ProductList = ({ products, toggleFav }: ProductListProps) => {
+  return (
+		<>
+			{products.length > 0
+			  ? (
+			  products.map((item, index) => (
+						<ProductItem key={item.id} index={index} {...item} toggleFav={toggleFav} />
+			  ))
+			    )
+			  : (
+					<p>No Data Avaliable</p>
+			    )}
+		</>
+  )
+}
+
+
+export default ProductList
